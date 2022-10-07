f5acf79f13c6a8bd11d97a532e0848712cb829cc
FeryKevin <90443608+FeryKevin@users.noreply.github.com>
2022-10-07 23:02:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="js/script.js"></script>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    
+    <body>
+        
+        <?php require 'php/navbar.php' ?>
+        
+        <section id="sec1">
+            
+            <?php require 'php/menu.php' ?>
+            
+        </section>
+        
+        <?php require 'php/footer.php' ?>
+        
+    </body>
+    
+</html>
---
+++ php/navbar.php
@@ -0,0 +1,8 @@
+<div id='navbar'>
+    <div>
+        <img src="img/logo.png" class="navbar-brand">    
+        <span class="glyphicon glyphicon-menu-hamburger" style="color: #fff" id="gly1"></span>          
+    </div>
+</div>
+
+
