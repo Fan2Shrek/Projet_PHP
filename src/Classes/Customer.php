33fd2f97c8b9ab12113e01a50764e3eb76539701
Fan2Shrek <pierre27.ambroise@gmail.com>
2022-10-10 14:50:36 +0200

    Maj Costumer

--- src/Classes/Customer.php
+++ src/Classes/Customer.php
@@ -8,6 +8,7 @@ use src\Traits\NameTrait;
 use src\Interfaces\codeNoteInterface;
 use src\Interfaces\nameInterface;
 use src\Interfaces\IdInterface;
+use src\Form\Connection;
 
 class Customer implements IdInterface, nameInterface, codeNoteInterface{
     use idTrait, NameTrait, codeNotesTrait;
@@ -19,4 +20,19 @@ class Customer implements IdInterface, nameInterface, codeNoteInterface{
     )
     {
     }
-}
+    
+    public function addCustomer(): bool{
+        try{
+            $database = Database::connect();
+            $statement = $database.prepare('INSERT INTO customer VALUES (=:code, =:name, =:notes)');
+            $statement->bindParam(':code', $this->code);
+            $statement->bindParam(':name', $this->name);
+            $statement->bindParam(':notes', $this->notes);
+            $statement.execute();
+            return true;
+        }
+        catch{
+            return false;
+        }
+    }
+}   
