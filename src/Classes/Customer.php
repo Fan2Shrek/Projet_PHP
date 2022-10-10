611f75698de398739723666ee54427f734ae1f58
Fan2Shrek <pierre27.ambroise@gmail.com>
2022-10-10 15:51:51 +0200

    Possibilité d'ajout d'un client

--- src/Classes/Customer.php
+++ src/Classes/Customer.php
@@ -8,7 +8,8 @@ use src\Traits\NameTrait;
 use src\Interfaces\CodeNoteInterface;
 use src\Interfaces\NameInterface;
 use src\Interfaces\IdInterface;
-use src\Form\Connection;
+use src\Php\Connection;
+use FFI\Exception;
 
 class Customer implements IdInterface, nameInterface, codeNoteInterface{
     use idTrait, NameTrait, codeNotesTrait;
@@ -21,19 +22,18 @@ class Customer implements IdInterface, nameInterface, codeNoteInterface{
     {
     }
     
-    /*
     public function addCustomer(): bool{
         try{
-            $database = Database::connect();
-            $statement = $database.prepare('INSERT INTO customer VALUES (=:code, =:name, =:notes)');
-            $statement->bindParam(':code', $this->code);
-            $statement->bindParam(':name', $this->name);
-            $statement->bindParam(':notes', $this->notes);
-            $statement.execute();
+            $database = Connection::connect();
+            $statement = $database->prepare('INSERT INTO customer (code, name, notes) VALUES (?, ?, ?)');
+            $statement->bindParam(1, $this->code);
+            $statement->bindParam(2, $this->name);
+            $statement->bindParam(3, $this->notes);
+            $statement->execute();
             return true;
         }catch (Exception $e)
         {
             return false;
         }
-    }*/
+    }
 }   
