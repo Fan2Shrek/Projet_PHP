32b5ae72cc22c1c1886ffc4990f57c27da09e5cf
Fan2Shrek <pierre27.ambroise@gmail.com>
2022-10-10 16:26:28 +0200

    Rename layout

--- php/menu.php
+++
@@ -1,6 +0,0 @@
-<div id='menu'> 
-    <ul id="m1"><a href="">&emsp;<span class="glyphicon glyphicon-home"></span>&emsp;TABLEAU DE BORD</a></ul>
-    <ul id="m2"><a href="">&emsp;<span class="glyphicon glyphicon-user"></span>&emsp;PROJETS</a></ul>
-    <ul id="m3"><a href="">&emsp;<span class="glyphicon glyphicon-inbox"></span>&emsp;CLIENTS</a></ul>
-    <ul id="m4"><a href="">&emsp;<span class="glyphicon glyphicon-check"></span>&emsp;HÉBERGEURS</a></ul>
-</div>
