79719256017890ca81c58b5b874428c6e29cd944
Fan2Shrek <pierre27.ambroise@gmail.com>
2022-10-14 09:17:38 +0200

    Ajout de la modification d'un customer

--- src/Classes/Customer.php
+++ src/Classes/Customer.php
@@ -22,18 +22,22 @@ class Customer implements IdInterface, nameInterface, codeNoteInterface{
     {
     }
     
-    public function addCustomer(): bool{
-        try{
-            $database = Connection::connect();
-            $statement = $database->prepare('INSERT INTO customer (code, name, notes) VALUES (?, ?, ?)');
-            $statement->bindParam(1, $this->code);
-            $statement->bindParam(2, $this->name);
-            $statement->bindParam(3, $this->notes);
-            $statement->execute();
-            return true;
-        }catch (Exception $e)
-        {
-            return false;
-        }
+    public function addCustomer(): void{
+        $database = Connection::connect();
+        $statement = $database->prepare('INSERT INTO customer (code, name, notes) VALUES (?, ?, ?)');
+        $statement->bindParam(1, $this->code);
+        $statement->bindParam(2, $this->name);
+        $statement->bindParam(3, $this->notes);
+        $statement->execute();
+    }
+
+    public function UpdateCustomer (Customer $newCus): void{
+        $database = Connection::connect();
+        $statement = $database->prepare ('UPDATE customer set code = ? , name = ?, notes = ? WHERE id= ?');
+        $statement->bindParam(1, $newCus->getCode());
+        $statement->bindParam(2, $newCus->getName());
+        $statement->bindParam(3, $newCus->getNotes());
+        $statement->bindParam(4, $this->id);
+        $statement->execute();
     }
 }
