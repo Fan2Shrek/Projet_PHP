36df4ebf9262f6efd05c1ffc01c1e2b274f86fe3
FeryKevin <kevin.fery@outlook.com>
2022-10-17 13:00:48 +0200

    Evolution de l'arborescence

--- Customers/Update.php
+++ Customers/Update.php
@@ -34,12 +34,13 @@ if (isset($_POST['submit_delete'])){
 <!DOCTYPE html>
 <html>
     <head>
+        <base href='../'>
         <title>Modifier un client</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="../js/script.js"></script>
-        <link rel="stylesheet" href="../css/styles.css">
+        <link rel="stylesheet" href="css/styles.css">
     </head>
     
     <body>
