319076201f5a4bad273ae2fca6b1ba9d98f14dd5
FeryKevin <kevin.fery@outlook.com>
2022-10-17 12:16:17 +0200

    Evolution de l'arborescence

--- Customers/Update.php
+++ Customers/Update.php
@@ -1,25 +1,32 @@
-<?php
+<?php 
 
-require 'src/autoloader.php';
+require '../src/autoloader.php';
 
 use App\Classes\Customer;
 use App\Repository\CustomerRepository;
 use App\Forms\Validator;
 
-$errors = array();
+if (isset($_GET['id'])){
+    $customer = CustomerRepository::getCustomerById($_GET['id']);
+}
 
 if (isset($_POST['submit'])){
     $code = 'CUST_'. $_POST['name'];
-    $customer = new Customer(0,
+    $newCustomer = new Customer(0,
     $code,
     $_POST['name'],
     $_POST['notes']);
-
-    $errors = Validator::checkCustomer($customer);
+    $errors = Validator::checkCustomer($newCustomer);
     if (null === $errors){
-        CustomerRepository::addCustomer($customer);
-        header("Location: view.php");
+        CustomerRepository::updateCustomer($customer, $newCustomer);
+        header("Location: ../View_Customers.php");
     }
+}
+
+if (isset($_POST['submit_delete'])){
+    $customer = CustomerRepository::getCustomerById($_GET['id']);
+    CustomerRepository::deleteCustomer($customer);
+    header('Location: ../View_Customers.php');
 }
 
 ?>
@@ -27,17 +34,17 @@ if (isset($_POST['submit'])){
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Ajouter un client</title>
+        <title>Modifier un client</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <script src="js/script.js"></script>
-        <link rel="stylesheet" href="css/styles.css">
+        <script src="../js/script.js"></script>
+        <link rel="stylesheet" href="../css/styles.css">
     </head>
     
     <body>
         
-        <?php require 'layout/navbar.php' ?>
+        <?php require '../layout/navbar.php' ?>
         
         <section id="insertClient">
 
@@ -45,8 +52,8 @@ if (isset($_POST['submit'])){
                 <div class="row">
 
                     
-                    <div class="col-lg-3 col-md-3 col-sm-6">
-                        <?php require 'layout/menu.php' ?>
+                    <div class="col-lg-3 col-md-3 col-sm-12">
+                        <?php require '../layout/menu.php' ?>
                     </div>
 
                     <!-- titre -->
@@ -54,9 +61,7 @@ if (isset($_POST['submit'])){
 
                         <!-- section -->
                         <div class="col-lg-12 col-md-12 col-sm-12">
-
-                        <h3 class="nouv">Nouveau client</h3>
-
+                            <h3 class="nouv"><?php echo $customer->getName();?></h3>
                             <div class="infoGenerale">
                                 <p><strong>INFORMATIONS GÉNÉRALES</strong></p>
                             </div>
@@ -70,30 +75,36 @@ if (isset($_POST['submit'])){
                                 <form method='Post'>
 
                                     <label class="lab">Nom <span style="color:red">*</span></label>
-                                    <input name='name' class="AddClient" value="<?php echo (!isset($_POST['name']))? '' : $_POST['name'] ?>">
+                                    <input name='name' class="AddClient" value="<?php echo $customer->getName(); ?>">
                                     <p class="error"><?php echo (!isset($errors['nameError']))? '' : $errors['nameError'] ?></p>
 
                                     <br>
 
                                     <label class="lab">Code interne</label>
-                                    <button disabled="disabled" class="AddClient1">Champs généré automatiquement</button>
+                                    <input size="30" disabled="disabled" class="UpClient" value="<?php echo $customer->getCode(); ?>">
 
                                     <br>
 
                                     <label class="lab2">Notes / remarques</label>
-                                    <textarea name='notes' class="AddClient2"><?php echo (!isset($_POST['notes']))? '' : $_POST['notes'] ?></textarea>
+                                    <textarea name='notes' class="AddClient2"><?php echo $customer->getNotes(); ?></textarea>
                                     <p class="error"><?php echo (!isset($errors['notesError']))? '' : $errors['notesError'] ?></p>
 
                                     <!-- bouton form -->
                                     <div class="col-lg-12 col-md-12 col-sm-12">
-                                        <div class="btnAdd">
-                                            <a href="view.php" class="btnInsert1">Annuler</a>&emsp;
-                                            <button type='submit' name='submit' class="btnInsertSave"><span class="glyphicon glyphicon-ok"></span> Sauvegarder</button>
+                                        <div class="btnAdd3"> 
+                                        <a href="../View_Customers.php" class="btnInsert1">Annuler</a>&emsp;    
+                                        <button type='submit' name='submit' class="btnInsertSave"><span class="glyphicon glyphicon-ok"></span> Sauvegarder</button>
                                         </div>
+                                        <div class="btnAdd4"> 
+                                            <form method="post">
+                                                    <input type='hidden' value="<?php echo $_GET['id']?>">
+                                                    <button type='submit' name='submit_delete' class="btnInsertSave"><span class="glyphicon glyphicon-trash"></span> Supprimer</button>&emsp;
+                                            </form> 
+                                        </div>                                          
                                     </div>
 
                                 </form>
-
+                                
                             </div>
                         </div>
 
@@ -104,7 +115,7 @@ if (isset($_POST['submit'])){
 
         </section>
         
-        <?php require 'layout/footer.php' ?>
+        <?php require '../layout/footer.php' ?>
         
     </body>
 
