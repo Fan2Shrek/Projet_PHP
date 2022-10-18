79557660969f1aa62ef909bc21111dad1f74fed9
FeryKevin <kevin.fery@outlook.com>
2022-10-18 21:45:39 +0200

    Correctif lien public

--- Host/Insert.php
+++ Host/Insert.php
@@ -40,8 +40,8 @@ if (isset($_POST['submit'])){
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <script src="../js/script.js"></script>
-        <link rel="stylesheet" href="css/styles.css">
+        <script src="public/js/script.js"></script>
+        <link rel="stylesheet" href="public/css/styles.css">
     </head>
     
     <body>
