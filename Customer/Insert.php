277f0d8d4b8ce828eaf02960ceb243807dbac24c
FeryKevin <kevin.fery@outlook.com>
2022-10-25 22:42:29 +0200

    updated front insert customer

--- Customer/Insert.php
+++ Customer/Insert.php
@@ -48,6 +48,7 @@ if (isset($_POST['submit'])){
     
     <body>
         
+        <!-- navbar -->
         <?php require '../layout/navbar.php' ?>
         
         <section id="insert">
@@ -55,51 +56,50 @@ if (isset($_POST['submit'])){
             <div class="container-fluid">
                 <div class="row">
 
-                    
+                    <!-- menu -->
                     <div class="col-lg-3 col-md-3 col-sm-6">
                         <?php require '../layout/menu.php' ?>
                     </div>
 
-                    <!-- titre -->
+                    <!-- page -->
                     <div class="col-lg-9 col-md-9 col-sm-12">
 
-                        <!-- section -->
+                        <!-- lien -->
                         <div class="col-lg-12 col-md-12 col-sm-12">
-
-                        <h3 class="nouv">Nouveau client</h3>
-
-                            <div class="infoGenerale">
-                                <p><strong>INFORMATIONS GÉNÉRALES</strong></p>
-                            </div>
+                            <h2 class="nouv">Nouveau client</h2>
+                            <ul class="listContact">
+                                <a href="Customer/view.php" class="infoGenerale">INFORMATIONS GÉNÉRALES</a>&emsp;
+                                <a href="Contact/view.php" class="contactLien">CONTACTS CLIENT</a>
+                            </ul>
                         </div>
 
                         <!-- debut carré -->
                         <div class="col-lg-12 col-md-12 col-sm-12">
-                            <div class="addClient">
+                            <div class="add">
 
                                 <!-- début form -->
                                 <form method='Post'>
 
                                     <label class="lab">Nom <span style="color:red">*</span></label>
-                                    <input name='name' class="AddClient" value="<?php echo (!isset($_POST['name']))? '' : $_POST['name'] ?>">
+                                    <input name='name' class="input0" value="<?php echo (!isset($_POST['name']))? '' : $_POST['name'] ?>">
                                     <p class="error"><?php echo (!isset($errors['nameError']))? '' : $errors['nameError'] ?></p>
 
                                     <br>
 
                                     <label class="lab">Code interne</label>
-                                    <button disabled="disabled" class="AddClient1">Champs généré automatiquement</button>
+                                    <button disabled="disabled" class="input1">Champs généré automatiquement</button>
 
                                     <br>
 
                                     <label class="lab2">Notes / remarques</label>
-                                    <textarea name='notes' class="AddClient2"><?php echo (!isset($_POST['notes']))? '' : $_POST['notes'] ?></textarea>
+                                    <textarea name='notes' class="textarea"><?php echo (!isset($_POST['notes']))? '' : $_POST['notes'] ?></textarea>
                                     <p class="error"><?php echo (!isset($errors['notesError']))? '' : $errors['notesError'] ?></p>
 
                                     <!-- bouton form -->
                                     <div class="col-lg-12 col-md-12 col-sm-12">
-                                        <div class="btnAdd5">
-                                            <a href="Customer/View.php" class="btnInsert1">Annuler</a>&emsp;
-                                            <button type='submit' name='submit' class="btnInsertSave"><span class="glyphicon glyphicon-ok"></span> Sauvegarder</button>
+                                        <div class="btnPlace">
+                                            <a href="Customer/View.php" class="btnBlanc">ANNULER</a>&emsp;
+                                            <button type='submit' name='submit' class="btnOrange"><span class="glyphicon glyphicon-ok"></span> SAUVEGARDER</button>
                                         </div>
                                     </div>
 
@@ -115,6 +115,7 @@ if (isset($_POST['submit'])){
 
         </section>
         
+        <!-- footer -->
         <?php require '../layout/footer.php' ?>
         
     </body>
